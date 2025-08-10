be23fc2710e6addf052e91bbe558876a80921779
Kane005005 <traorekanz@gmail.com>
2025-08-10 09:34:00 +0000

    Update PortfolioSection.js

--- k-web/src/components/PortfolioSection.js
+++ k-web/src/components/PortfolioSection.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import './PortfolioSection.css';
 import { useInView } from 'react-intersection-observer';
+import './PortfolioSection.css';
 
 const PortfolioSection = () => {
   const [projects, setProjects] = useState([]);
@@ -12,7 +12,7 @@ const PortfolioSection = () => {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const response = await fetch('http://127.0.0.1:8000/api/projects/');
+        const response = await fetch('https://kweb.pythonanywhere.com/api/projects/');
         if (!response.ok) {
           throw new Error(`Erreur HTTP: ${response.status}`);
         }
@@ -32,32 +32,21 @@ const PortfolioSection = () => {
   return (
     <section className={`portfolio-section ${inView ? 'animate' : ''}`} id="portfolio" ref={ref}>
       <div className="container">
-        <h2 className="section-title">Quelques projets déjà réalisés</h2>
+        <h2 className="section-title">Quelques-unes de nos réalisations</h2>
         <p className="section-subtitle">
           Voici un aperçu de mon travail. Un aperçu de ce que l'on pourrait créer ensemble.
         </p>
         <div className="portfolio-grid">
           {loading && <p>Chargement des projets...</p>}
           {error && <p>Une erreur est survenue : {error}</p>}
-
+          
           {!loading && !error && projects.length > 0 ? (
             projects.map((project, index) => (
               <div className="portfolio-card" key={index}>
-                <div className="portfolio-image-container">
-                  {/* L'URL de l'image vient maintenant de l'API */}
-                  <img src={`http://127.0.0.1:8000${project.image}`} alt={project.title} className="portfolio-image" />
-                </div>
-                <div className="portfolio-info">
-                  <h3 className="portfolio-title">{project.title}</h3>
-                  <p className="portfolio-type">{project.type}</p>
-                  <div className="portfolio-stack">
-                    {project.stack.map((tech, i) => (
-                      <span className="stack-tag" key={i}>{tech}</span>
-                    ))}
-                  </div>
-                  <a href={project.link} target="_blank" rel="noopener noreferrer" className="view-project-btn">
-                    Voir en ligne
-                  </a>
+                <img src={`https://kweb.pythonanywhere.com${project.image}`} alt={project.title} className="portfolio-image" />
+                <div className="card-info">
+                  <h3 className="card-title">{project.title}</h3>
+                  <p className="card-type">{project.type}</p>
                 </div>
               </div>
             ))
