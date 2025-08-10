d509887eccee7b4309a8e247f86b9785c08c12c9
Kane005005 <traorekanz@gmail.com>
2025-08-10 08:56:07 +0000

    Update urls.py

--- k-web-backend/kweb_project/urls.py
+++ k-web-backend/kweb_project/urls.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path # Import re_path
+from django.views.generic import TemplateView # Import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('portfolio.urls')), # <-- URLs de ton API Django
-    path('', TemplateView.as_view(template_name='index.html')), # <-- Servira l'app React
+    path('api/', include('portfolio.urls')),
+
+    # Add the following URL patterns for serving static and media files in development
+    # You might not need these on PythonAnywhere if you've configured them in the 'Web' tab
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    # This catch-all URL pattern must be the LAST one in the list.
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
